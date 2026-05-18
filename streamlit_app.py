fc769693ab252aadc0e700270b67a689ad95f36c
dodiprayuda8-lab <dodiprayuda8@gmail.com>
2026-05-18 12:09:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 ACA LEMAH ")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "ciu doang udah kayak mau mati [docs.streamlit.io](https://docs.streamlit.io/)."
 )
